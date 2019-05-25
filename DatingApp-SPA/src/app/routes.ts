7eb34c8fe0cd57939fd3fb56ca8ceb3459df97bf
unknown <mariaus.vai@gmail.com>
2019-05-25 21:27:50 +0200

    Added edit profile

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,3 +1,4 @@
+import { MemberEditResolver } from './_resolves/member-edit.resolver';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { HomeComponent } from './home/home.component';
 import {Routes} from '@angular/router';
@@ -7,6 +8,8 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailResolver } from './_resolves/member-detail.resolver';
 import { MemberListResolver } from './_resolves/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
@@ -17,6 +20,8 @@ export const appRoutes: Routes = [
         children: [
             { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
             { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+            { path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver},
+        canDeactivate: [PreventUnsavedChanges]},
             { path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent},
         ]
